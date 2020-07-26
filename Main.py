16576faefa05bff3313b26fb2d5d9283c12de089
pravindesai__ <pravindesai100@gmail.com>
2020-07-26 17:05:04 +0530

    Some Fixes
    .

--- Main.py
+++ Main.py
@@ -1,62 +1,57 @@
-from distutils import command
 import sounddevice as sd
 import speech_recognition as sr
 from scipy.io.wavfile import write
 import numpy as np
-import os
 import webbrowser
-import subprocess
+
+
+def Myreco():
+ fs = 48000
+ seconds = 5
+ myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
+ sd.wait()
+ Savef(fs, myrecording)
+
+
+def Savef(fs, myrecording):
+ y = (np.iinfo(np.int32).max * (myrecording / np.abs(myrecording).max())).astype(np.int32)
+ write("Audio_File1.wav", fs, y)
+ Convert()
+
+
+def Convert():
+ r = sr.Recognizer()
+ with sr.AudioFile('Audio_File1.wav') as source:
+  audio = r.record(source)
+ try:
+  command = r.recognize_google(audio, language='en-US')
+  Webb(command)
+ except:
+  Webb("How to use Speech recognizer")
+
+
+
+def Webb(command):
+ webbrowser.open('https://www.google.com/search?source=hp&ei=Pk8TX_HkNfrtz7sP9raH0AE&q=' + command, new=2)
+
 
 print("Enter the Hi ya Hello")
 
 
 key =    {
-     'hi':' If you want Search any think so... Speck',
-     'hello':' If you want Search any think so... Speck'
+     'hi':' If you want Search any think so... Speak',
+     'hello':' If you want Search any think so... Speak'
     }
 
 while True:
 
- z=input()
- z = z.lower()
+ a=input()
+ a = a.lower()
  try:
-  a=key[z]
+  print(key[a])
+
  except:
-  a=None
   print("try agin")
 
-
  if a:
-
-  def Myreco():
-   frequency = 440
-   fs=48000
-   seconds=5
-
-   myrecording=sd.rec(int(seconds*fs), samplerate=fs, channels=2)
-   sd.wait()
-   Savef(fs,myrecording)
-   # write('Audio_File.wav',fs, myrecording)
-
-  def Savef(fs,myrecording):
-   y=(np.iinfo(np.int32).max * (myrecording/np.abs(myrecording).max())).astype(np.int32)
-   write("Audio_File1.wav", fs, y)
-   Convert()
-
-  def Convert():
-   r=sr.Recognizer()
-   with sr.AudioFile('Audio_File1.wav') as source:
-     audio=r.record(source)
-   command=r.recognize_google(audio, language='en-US')
-   #Webb(command)
-   command=command.lower()
-   #os.system(command)
-   def Webb(command):
-    webbrowser.open('https://www.google.com/search?source=hp&ei=Pk8TX_HkNfrtz7sP9raH0AE&q='+command, new=2)
-
-  #Myreco()
-   #os.system(command)
-   #command=subprocess.call("ls -l")
-
- #else:
-  #print("Pleage try again")
+  Myreco()
